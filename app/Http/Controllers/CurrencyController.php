9b4abc1e207e91b85670fcd466447994b6382c55
Samala Sumanth <chintusamala96@gmail.com>
2023-03-05 23:59:13 +0000

    Caching Technique

--- app/Http/Controllers/CurrencyController.php
+++ app/Http/Controllers/CurrencyController.php
@@ -2,16 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 
 class CurrencyController extends Controller
 {
     public function index()
     {
-        $response = Http::accept('application/json')->withHeaders(['apikey' => env('CURRENCY_API_KEY')])->get('https://api.apilayer.com/exchangerates_data/latest', [
+        // Can be implemented with memcache or redis caching
+        $end_point = "https://api.apilayer.com/exchangerates_data/latest";
+        $cached_response = Cache::store('file')->get('response');
+        if (isset($cached_response)) {
+            return $cached_response;
+        }
+        $response = Http::accept('application/json')->withHeaders(['apikey' => 'Nx26GuQ2puzwWGVSygLYMsG3FnrrqVjB'])->get($end_point, [
             'symbols' => "USD, EUR, CHF, AUD, CAD",
             'base' => 'GBP'
         ]);
-        return $response->json();
+        $result = $response->json();
+        // 120 two minutes caching
+        Cache::store('file')->put('response', $result, 120);
+        return $result;
     }
 }
